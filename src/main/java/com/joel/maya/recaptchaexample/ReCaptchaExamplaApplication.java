f499a7abd29c21664a18162a02d4ec1eeedc399c
Joel Miguel Maya Castrejón <mike.maya@ciencias.unam.mx>
2023-04-23 18:24:18 -0600

    Empezando a crear aplicacion que es un registro basico de usuarios que utiliza reCaptcha V3.

---
+++ src/main/java/com/joel/maya/recaptchaexample/ReCaptchaExamplaApplication.java
@@ -0,0 +1,13 @@
+package com.joel.maya.recaptchaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReCaptchaExamplaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReCaptchaExamplaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joel/maya/recaptchaexample/ReCaptchaExamplaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joel.maya.recaptchaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReCaptchaExamplaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
